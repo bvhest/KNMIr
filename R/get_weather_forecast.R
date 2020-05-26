b80b4d8afb55d21ef2e19fa190f9e0534b657ca0
B van Hest <hestbv@gmail.com>
2020-05-26 23:31:05 +0200

    14 daagse voorspelling van Weerplaza aangepast zodat de voorspelling voor alle knmi stations kunnen worden opgevraagd.

--- R/get_weather_forecast.R
+++ R/get_weather_forecast.R
@@ -162,55 +162,61 @@ get_6day_weather_forecast <- function() {
 #' @source \url{https://www.weerplaza.nl/nederland/}
 #' @keywords weather forecast
 #' @export
-get_14day_weather_forecast <- function() {
+get_14day_weather_forecast <-
+  function(station = 260) {
+    # default station is 260 (De Bilt, KNMI)
 
-   # haal 6-daagse voorspelling:
-   baseURL <- "https://www.weerplaza.nl/nederland/gilze-rijen/9950/"
+    # haal 6-daagse voorspelling:
+    base_URL <- "https://www.weerplaza.nl/nederland/"
+    location_URL <-
+     get_URL_based_onlocation(station)
+    URL <-
+     paste0(base_URL, location_URL)
 
-   pagina <- xml2::read_html(baseURL)
-   weather_table <-
+    pagina <- xml2::read_html(URL)
+    weather_table <-
      rvest::html_nodes(pagina,
                        xpath = '//table[@class="hasChart" or @class=""]')
-   weather_row <-
+    weather_row <-
      rvest::html_nodes(weather_table,
                        xpath = '//table[@class="hasChart" or @class=""]/tbody/tr')
-   # 10 rijen:
-   # rij 1: datum + samenvatting
-   # rij 2: zonkans
-   # rij 3: min/max temp
-   # rij 4: neerslag (kans en hoeveelheid)
-   # rij 5: wind
-   # idem voor rij 6-10
-   #
-   # Nb elke rij bevat in de td-elementen een attribuut met de datum (@data-day)
+    # 10 rijen:
+    # rij 1: datum + samenvatting
+    # rij 2: zonkans
+    # rij 3: min/max temp
+    # rij 4: neerslag (kans en hoeveelheid)
+    # rij 5: wind
+    # idem voor rij 6-10
+    #
+    # Nb elke rij bevat in de td-elementen een attribuut met de datum (@data-day)
 
-   # extraheer waarden in lists:
-   datum <-
+    # extraheer waarden in lists:
+    datum <-
      rvest::html_nodes(weather_row,
                        xpath = 'td/@data-day')
-   maxTemp <-
+    maxTemp <-
      rvest::html_nodes(weather_row,
                        xpath = 'td/div[@class="red temp"]')
-   minTemp <-
+    minTemp <-
      rvest::html_nodes(weather_row,
                        xpath = 'td/div[@class="blue temp"]')
-   neerslag <-
+    neerslag <-
      rvest::html_nodes(weather_row,
                        xpath = 'td/div[@class="blue"][not(i/@class)]')
-   neerslagKans <-
+    neerslagKans <-
      rvest::html_nodes(weather_row,
                        xpath = 'td/div[@class="blue"][i/@class="fa fa-tint blue"]')
-   percZon <-
+    percZon <-
      rvest::html_nodes(weather_row,
                        xpath = 'td/div[@class="orange"]')
-   wind <-
+    wind <-
      rvest::html_nodes(weather_row,
                        xpath = 'td/div[@class="hide"]')
 
-   datum <- unique(rvest::html_text(datum))
-   size <- length(datum)
-   voorspelling <-
-     data.frame(station = rep("260", size),
+    datum <- unique(rvest::html_text(datum))
+    size <- length(datum)
+    voorspelling <-
+     data.frame(station = rep(station, size),
                 YYYYMMDD = rep("20000101", size),
                 date = as.Date("2000-01-01") + 1:size, # just dummy dates
                 maxTemp = double(size),
@@ -226,8 +232,8 @@ get_14day_weather_forecast <- function() {
                 windkracht = double(size),
                 stringsAsFactors = FALSE)
 
-   # nu alles in een loopje:
-   for (i in 1:size) {
+    # nu alles in een loopje:
+    for (i in 1:size) {
       voorspelling$date[i] <- as.Date(datum[i], format = "%d%m%Y")
       voorspelling$YYYYMMDD[i] <- as.character(voorspelling$date[i], format = "%Y%m%d")
       # maxTemp
@@ -249,25 +255,25 @@ get_14day_weather_forecast <- function() {
       voorspelling$percZon[i] <- mean(as.numeric(extraheer_numerieke_waarden(percZon[i])))
       # wind
       voorspelling$windkracht[i] <- mean(as.numeric(extraheer_numerieke_waarden(wind[i])))
-   }
+    }
 
-   # kolom-namen omzetten van leesbaar, naar  codes (kan weer worden teruggezet met de functie 'rename_columns()')
-   colnames(voorspelling) <-
+    # kolom-namen omzetten van leesbaar, naar  codes (kan weer worden teruggezet met de functie 'rename_columns()')
+    colnames(voorspelling) <-
      c("STN","YYYYMMDD","date","TX","SD_maxTemp","TN","SD_minTemp","TG","SD_gemTemp",
        "RH","SD_neerslag","neerslagKans","SP","windkracht")
 
-   # windkracht omzetten naar m/s:
-   wind <- converteer_beaufort_schaal(voorspelling$windkracht)
-   voorspelling <- cbind(voorspelling, wind)
+    # windkracht omzetten naar m/s:
+    wind <- converteer_beaufort_schaal(voorspelling$windkracht)
+    voorspelling <- cbind(voorspelling, wind)
 
-   # dezelfde hulp-kolommen toevoegen als in de andere functies die KNMI-data ophalen:
-   voorspelling$doy <- yday(voorspelling$date)
-   voorspelling$year <- year(voorspelling$date)
-   voorspelling$month <- month(voorspelling$date)
-   voorspelling$week <- week(voorspelling$date)
-   voorspelling$day <- wday(voorspelling$date)
+    # dezelfde hulp-kolommen toevoegen als in de andere functies die KNMI-data ophalen:
+    voorspelling$doy <- yday(voorspelling$date)
+    voorspelling$year <- year(voorspelling$date)
+    voorspelling$month <- month(voorspelling$date)
+    voorspelling$week <- week(voorspelling$date)
+    voorspelling$day <- wday(voorspelling$date)
 
-   return(voorspelling)
+    return(voorspelling)
 }
 
 ########################################################################################################################
@@ -293,3 +299,56 @@ converteer_beaufort_schaal <- function(windkracht) {
   return(data.frame(minWind = min_m_per_s, maxWind = max_m_per_s))
 }
 
+# decode station id of naam voor Weerplaza URL
+get_URL_based_onlocation <-
+  function(station) {
+
+    locationURL <-
+      dplyr::case_when(station == 210 | station == "Valkenburg" ~ "valkenburg/19412/",
+                       station == 215 | station == "Voorschoten" ~ "voorschoten/19838/",
+                       station == 225 | station == "IJmuiden" ~ "ijmuiden/11054/",
+                       station == 235 | station == "De Kooy" ~ "de-kooy/2753344/",
+                       station == 240 | station == "Schiphol" ~ "schiphol/5577/",
+                       station == 242 | station == "Vlieland" ~ "vlieland/2743351/",
+                       station == 248 | station == "Wijdenes" ~ "wijdenes/20205/",
+                       station == 249 | station == "Berkhout" ~ "berkhout/6569/",
+                       station == 251 | station == "Hoorn Terschelling" ~ "hoorn/2750195/",
+                       station == 257 | station == "Wijk aan Zee" ~ "wijk-aan-zee/20209/",
+                       station == 260 | station == "De Bilt" ~ "de-bilt/8500/",
+                       station == 265 | station == "Soesterberg" ~ "amersfoort/5551/",
+                       station == 267 | station == "Stavoren" ~ "stavoren/18367/",
+                       station == 269 | station == "Lelystad" ~ "lelystad/12571/",
+                       station == 270 | station == "Leeuwarden" ~ "leeuwarden/12537/",
+                       station == 273 | station == "Marknesse" ~ "marknesse/13455/",
+                       station == 275 | station == "Deelen" ~ "deelen/8552/",
+                       station == 277 | station == "Lauwersoog" ~ "lauwersoog/12418/",
+                       station == 278 | station == "Heino" ~ "heino/10580/",
+                       station == 279 | station == "Hoogeveen" ~ "hoogeveen/10870/",
+                       station == 280 | station == "Eelde" ~ "eelde-paterswolde/8967/",
+                       station == 283 | station == "Hupsel" ~ "hupsel/2749993/",
+                       station == 286 | station == "Nieuw Beerta" ~ "nieuw-beerta/2747446/",
+                       station == 290 | station == "Twenthe" | station == "Twente" ~ "twente/2743881/",
+                       station == 308 | station == "Cadzand" ~ "cadzand/7340/",
+                       station == 310 | station == "Vlissingen" ~ "vlissingen/19806/",
+                       station == 311 | station == "Hoofdplaat" ~ "hoofdplaat/10860/",
+                       station == 315 | station == "Hansweert" ~ "hansweert/10434/",
+                       station == 319 | station == "Westdorpe" ~ "westdorpe/20115/",
+                       station == 323 | station == "Wilhelminadorp" ~ "wilhelminadorp/20231/",
+                       station == 324 | station == "Stavenisse" ~ "stavenisse/18364/",
+                       station == 330 | station == "Hoek van Holland" ~ "hoek-van-holland/10780/",
+                       station == 331 | station == "Tholen" ~ "tholen/18860/",
+                       station == 340 | station == "Woensdrecht" ~ "woensdrecht/20308/",
+                       station == 343 | station == "Rotterdam Geulhaven" ~ "rotterdam/16707/",
+                       station == 344 | station == "Rotterdam" ~ "rotterdam/16707/",
+                       station == 348 | station == "Cabauw Mast" ~ "cabauw/13004/",
+                       station == 350 | station == "Gilze-Rijen" ~ "gilze-rijen/9950/",
+                       station == 356 | station == "Herwijnen" ~ "herwijnen/10680/",
+                       station == 370 | station == "Eindhoven" ~ "eindhoven/9020/",
+                       station == 375 | station == "Volkel" ~ "volkel/19829/",
+                       station == 377 | station == "Ell" ~ "ell/9091/",
+                       station == 380 | station == "Maastricht" ~ "maastricht/13193/",
+                       station == 391 | station == "Arcen" ~ "arcen/5741/",
+                       TRUE   ~ "de-bilt/8500/")
+
+    return(locationURL)
+  }
--- todo.md
+++ todo.md
@@ -1,11 +1,13 @@
 ToDo:
 
   - DONE: add the data from the sea-based stations
-  - turn station-parameter in stations-parameter (so that a list of stations can be downloaded in one call)
+  - change station-parameter to stations-parameter (so that a list of stations can be downloaded in one call)
   - DONE: retrieve the six day forecast from the KNMI-website.
   - provide English names for the variable-codes, next to the Dutch translations.
   - change source of KNMI stations to http://projects.knmi.nl/datacentrum/catalogus/catalogus/catalogus-gegevens-overzicht.html
-
+  - remove as many package dependencies as possible,
+    change to the tidyverse-packages.
+  - DONE: corrigeer get_xxday_weather_forecast: maak weerstation configurabel
 
 Bugs
 
@@ -16,3 +18,5 @@ Bugs
 	Error in KNMIr::get_climate_data_zip(stationID = "260", from = "19000101") : 
 	  object 'station' not found
 
+ 2. Issue with column-names (and number) for hourly API.
+ 
