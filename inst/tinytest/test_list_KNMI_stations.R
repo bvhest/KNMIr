05d0e3bcb24fd944b21b712561a6bd66683c0555
B van Hest <hestbv@gmail.com>
2020-07-06 22:10:58 +0200

    aangepast aan bijgewerkte lijst met KNMI stations.

--- inst/tinytest/test_list_KNMI_stations.R
+++ inst/tinytest/test_list_KNMI_stations.R
@@ -1,6 +1,6 @@
 # check number of available KNMI stations
-expect_equal(nrow(list_stations()), 45)
-expect_equal(nrow(list_stations(is_active = FALSE)), 48)
+expect_equal(nrow(list_stations()), 60)
+expect_equal(nrow(list_stations(is_active = FALSE)), 63)
 
 # check number of returned columns
 expect_equal(ncol(list_stations()), 8)
